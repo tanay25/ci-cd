86c047893c2a7720df0c87b13e3574404e62d338
root <root@ip-172-31-33-105.ap-south-1.compute.internal>
2026-02-07 07:56:06 +0000

    First Time 1

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
-
 @app.route("/")
 def home():
-    return "Secure CI/CD Pipeline Running!"
+    return "CI CD Security Pipeline"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    host = os.getenv("FLASK_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_PORT", 5000))
+    app.run(host=host, port=port)
 
